31f37a4575517eb02d72df489e4cb6a24e541742
Carlos Tojal <carlos.tojal@hotmail.com>
2023-03-19 17:16:33 +0000

    detect wheel slip

--- include/ackermann_state_estimator/estimator_node.hpp
+++ include/ackermann_state_estimator/estimator_node.hpp
@@ -1,19 +1,64 @@
 #include "rclcpp/rclcpp.hpp"
 #include "ackermann_msgs/msg/ackermann_drive.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/float64.hpp"
 #include <chrono>
 
+#define WHEEL_RADIUS 0.175
+#define WHEEL_BASE 1.58
+#define TRACK 1.22
+
+// acceptable percentage of wheel speed ratio to be considered slipping
+#define SLIP_THRESHOLD 0.8
+
 using namespace std::chrono_literals;
+
+struct wheel_speed {
+	long long timestamp;
+	double expected;
+	double measured;
+	double ratio;
+	bool set = false;
+};
 
 class EstimatorNode : public rclcpp::Node {
 	public:
 		EstimatorNode();
 
 	private:
-		void odom_callback();
-		void ackermann_callback(const ackermann_msgs::msg::AckermannDrive::SharedPtr msg) const;
+		geometry_msgs::msg::Twist cur_twist;
+		ackermann_msgs::msg::AckermannDrive cur_ackermann;
+		bool ackermann_received = false;
+
+		// [0][x] -> left wheels
+		// [1][x] -> right wheels
+		struct wheel_speed wheel_speeds[2][2];
+
+		// ackermann subscriber method
+		void ackermann_callback(const ackermann_msgs::msg::AckermannDrive::SharedPtr msg);
+
+		// wheel speeds subscriber methods
+		void left_front_wheel_callback(const std_msgs::msg::Float64::SharedPtr msg);
+		void right_front_wheel_callback(const std_msgs::msg::Float64::SharedPtr msg);
+		void left_back_wheel_callback(const std_msgs::msg::Float64::SharedPtr msg);
+		void right_back_wheel_callback(const std_msgs::msg::Float64::SharedPtr msg);
+
+		void get_cur_expected_wheel_speeds();
+		void get_cur_wheel_speeds_ratio();
+
+		bool are_all_wheel_speeds_set();
+
 		rclcpp::TimerBase::SharedPtr timer_;
+
+		// ackermann subscriber
 		rclcpp::Subscription<ackermann_msgs::msg::AckermannDrive>::SharedPtr ackermann_subscriber_;
-		// TODO: wheel speeds subscriber
+
+		// wheel speeds subscriber
+		rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr left_front_wheel_subscriber_;
+		rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr right_front_wheel_subscriber_;
+		rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr left_back_wheel_subscriber_;
+		rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr right_back_wheel_subscriber_;
+
+		// twist publisher
 		rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
 };
--- src/nodes/estimator_node.cpp
+++ src/nodes/estimator_node.cpp
@@ -5,14 +5,102 @@ EstimatorNode::EstimatorNode() : Node("estimator_node") {
 	publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("twist", 10);
 	ackermann_subscriber_ = this->create_subscription<ackermann_msgs::msg::AckermannDrive>(
 			"ackermann", 10, std::bind(&EstimatorNode::ackermann_callback, this, std::placeholders::_1));
+
+	// subscribe to wheel speeds
+	left_front_wheel_subscriber_ = this->create_subscription<std_msgs::msg::Float64>(
+			"left_front_wheel", 10, std::bind(&EstimatorNode::left_front_wheel_callback, this, std::placeholders::_1));
+	right_front_wheel_subscriber_ = this->create_subscription<std_msgs::msg::Float64>(
+			"right_front_wheel", 10, std::bind(&EstimatorNode::right_front_wheel_callback, this, std::placeholders::_1));
+	left_back_wheel_subscriber_ = this->create_subscription<std_msgs::msg::Float64>(
+			"left_back_wheel", 10, std::bind(&EstimatorNode::left_back_wheel_callback, this, std::placeholders::_1));
+	right_back_wheel_subscriber_ = this->create_subscription<std_msgs::msg::Float64>(
+			"right_back_wheel", 10, std::bind(&EstimatorNode::right_back_wheel_callback, this, std::placeholders::_1));	
 }
 
-void EstimatorNode::odom_callback() {
-	// TODO called on each jtimer tick to publish twist
+void EstimatorNode::ackermann_callback(const ackermann_msgs::msg::AckermannDrive::SharedPtr msg) {
+	this->cur_ackermann = *msg;
+	this->ackermann_received = true;
+
+	// TODO: check slip
+
+	// TODO: calculate twist
+
+	// TODO: publish twist
 }
 
-void EstimatorNode::ackermann_callback(const ackermann_msgs::msg::AckermannDrive::SharedPtr msg) const {
-	RCLCPP_INFO(this->get_logger(), "New ackermann message");
+void EstimatorNode::left_front_wheel_callback(const std_msgs::msg::Float64::SharedPtr msg) {
+	this->wheel_speeds[0][0].measured = msg->data;
+}
+
+void EstimatorNode::right_front_wheel_callback(const std_msgs::msg::Float64::SharedPtr msg) {
+	this->wheel_speeds[1][0].measured = msg->data;
+}
+
+void EstimatorNode::left_back_wheel_callback(const std_msgs::msg::Float64::SharedPtr msg) {
+	this->wheel_speeds[0][1].measured = msg->data;
+}
+
+void EstimatorNode::right_back_wheel_callback(const std_msgs::msg::Float64::SharedPtr msg) {
+	this->wheel_speeds[1][1].measured = msg->data;
+}
+
+// get the wheel speed of the car while driving on a straight line
+double get_wheel_speed_from_drive(double drive_speed, double wheel_radius) {
+	return drive_speed / wheel_radius;
+}
+
+// get a wheel speed from the drive speed, the wheel turning radius and the turning radius
+double get_wheel_speed_from_radiuses(double wheel_traj_radius, double turning_radius, double drive_speed) {
+	return drive_speed * wheel_traj_radius / turning_radius;
+}
+
+void EstimatorNode::get_cur_expected_wheel_speeds() {
+
+	if(this->cur_ackermann.steering_angle == 0) {
+		// if the steering angle is 0, the turning radius is infinite
+		#pragma omp parallel for collapse(2)
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 2; j++) {
+				this->wheel_speeds[i][j].expected = get_wheel_speed_from_drive(this->cur_ackermann.speed, WHEEL_RADIUS);
+			}
+		}
+	} else {
+		double turning_radius = WHEEL_BASE / tan(this->cur_ackermann.steering_angle);
+		double radius_diff = TRACK / 2 * sin(this->cur_ackermann.steering_angle);
+
+		int multiplier = this->cur_ackermann.steering_angle > 0 ? 1 : -1;
+
+		// calculate the left and right turning radiuses, based on the steering angle
+		#pragma omp parallel for collapse(2)
+		for(int i = 0; i < 2; i++) {
+			double wheel_traj_radius = turning_radius + (multiplier * radius_diff);
+			for(int j = 0; j < 2; j++) {
+				this->wheel_speeds[i][j].expected = get_wheel_speed_from_radiuses(wheel_traj_radius, turning_radius, this->cur_ackermann.speed);
+			}
+			multiplier *= -1;
+		}
+	}
+}
+
+void EstimatorNode::get_cur_wheel_speeds_ratio() {
+
+	#pragma omp parallel for collapse(2)
+	for(int i = 0; i < 2; i++) {
+		for(int j = 0; j < 2; j++) {
+			this->wheel_speeds[i][j].ratio = this->wheel_speeds[i][j].measured / this->wheel_speeds[i][j].expected;
+		}
+	}
+}
+
+bool EstimatorNode::are_all_wheel_speeds_set() {
+	for(int i = 0; i < 2; i++) {
+		for(int j = 0; j < 2; j++) {
+			if(!this->wheel_speeds[i][j].set) {
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 int main(int argc, char *argv[]) {
